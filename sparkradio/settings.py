6d8808c03b22dae9839b377ddb363d7681ef2fd9
Monil Jain <jainmonil2101@gmail.com>
2022-09-20 20:50:20 +0530

    Podcast Aggregator

--- sparkradio/settings.py
+++ sparkradio/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
