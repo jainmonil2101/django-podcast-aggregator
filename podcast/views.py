a2132bd6fb3a130dcbfbfc9a9740fcace065e313
Monil Jain <jainmonil2101@gmail.com>
2022-09-21 12:44:57 +0530

    Improved pagination & search functionality

--- podcast/views.py
+++ podcast/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Episode
 from django.core.paginator import Paginator
-from selenium import webdriver
-from sparkradio.settings import BASE_DIR
 
 # Create your views here.
 
 def index(request):
     if request.method == 'GET':
-        driver = webdriver.Chrome(BASE_DIR / 'chromedriver.exe')
-        url = driver.current_url
         search = request.GET.get('search', '')
         episodes = Episode.objects.filter(title__icontains=search).order_by('-published')
-        if search == "" or search == None or search == " ":
-            paginator = Paginator(episodes, per_page=10)
-        else:
-            paginator = Paginator(episodes, per_page=100)
-            
+        paginator = Paginator(episodes, per_page=10)    
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context = {   
