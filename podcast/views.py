b4fb477b6526f75536c0ac75a2ee7d95c89dcefa
Monil Jain <jainmonil2101@gmail.com>
2022-09-21 12:11:25 +0530

    Improved pagination & search functionality

--- podcast/views.py
+++ podcast/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Episode
 from django.core.paginator import Paginator
+from selenium import webdriver
 
 # Create your views here.
 
 def index(request):
     if request.method == 'GET':
+        driver = webdriver.Chrome()
+        url = driver.current_url
         search = request.GET.get('search', '')
         episodes = Episode.objects.filter(title__icontains=search).order_by('-published')
-        paginator = Paginator(episodes, per_page=10)
+        if search == "" or search == None or search == " ":
+            paginator = Paginator(episodes, per_page=10)
+        else:
+            paginator = Paginator(episodes, per_page=100)
+            
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context = {   
